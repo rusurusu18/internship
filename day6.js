49faf647c70b11a6670cf204095cc3b0302cae88
rushu <rushurushu721@gmail.com>
2026-06-28 18:16:55 +0545

    Update day6.js

--- day6.js
+++ day6.js
@@ -71,6 +71,26 @@ if (age >= 18) {
     console.log("Not elegible to vote.");
 }
 
+//if else if else
+let marks= 85;                      
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 80) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
+
+//else if
+let score= 75;
+if (score >= 90) {
+    console.log("Excellent");   
+} else if (score >= 75) {
+    console.log("Good");
+} else (score >= 60) {
+    console.log("Average");
+}
+
 //switch case
 let day= 3;
 switch (day) {

00204048c152bb704967af8fd308badbf8db2fd3
rushu <rushurushu721@gmail.com>
2026-06-30 22:15:28 +0545

    Assignment

---
+++ assignment.js
@@ -0,0 +1,13 @@
+let employee = {
+    id: "01",
+    name: "Ram",
+    department: "IT",
+    salary:"50,000"
+}
+console.log(employee);
+employee.address="Kathmandu";
+console.log(employee);
+employee.salary="60000";
+console.log(employee);
+delete employee.department;
+console.log(employee);

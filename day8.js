c0374878b3cfffef938f1e32648941283a71ab9d
rushu <rushurushu721@gmail.com>
2026-06-30 18:25:51 +0545

    day 8

---
+++ day8.js
@@ -0,0 +1,14 @@
+//while loop
+
+// while (condition) {
+//  checks condition before executing the block of code
+//     console.log("Hello World");
+// }
+
+//example of while loop
+
+let count=11;
+while(count<=10){
+    console.log("Number are" +count);
+    count++;
+}

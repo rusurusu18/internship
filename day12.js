3285fbde087f5702b740e8b38cdc36636dc9b654
rushu <rushurushu721@gmail.com>
2026-07-07 18:35:04 +0545

    day11.js

---
+++ day12.js
@@ -0,0 +1,17 @@
+//fetch api is a built in js api that is used to send http requests and receive responses from a server. It returns a promise that resolves to the response of the request.
+//fetch api is used to get data from the server and display it on the web page.
+
+//it is commonly used to:
+// - get data (get data (--GET))
+// - send data (send data (--POST))
+// - update data (update data (--PUT / PATCH))
+// - delete data  (delete data (--DELETE))
+
+// why do we use fetch api?
+// - easy to use and understand
+// - is built in js api
+// - it returns a promise that can be used to handle the response of the request
+// - it can be used to send and receive data from a server
+// - frondend needs data from the backend
+// - without this our website cannot communicate with the server
+

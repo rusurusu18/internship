ef2d2238f55b6af1c542a6b320697c8605406c11
rushu <rushurushu721@gmail.com>
2026-07-09 18:47:55 +0545

    day 13

---
+++ day13.js
@@ -0,0 +1,53 @@
+//arrow function
+//it is a compact syntax for writing function expressions using the => (arrow) operator.
+// An arrow function is a shorter and modern way to write a function in JavaScript.
+
+// const hello()=>{
+//     console.log ("hello world")
+// };
+// hello()
+
+// //
+// const sum=(a,b)=>{
+//     return a+b;
+// }
+// console.log(sum(10,12))
+
+// //template literals
+// //it is string enclosed in backicks(``)
+
+// let name="Rushu"
+// let surname="K.C."
+// console.log("name"+ name)
+// console.log(`My name is ${name} ${surname}`)
+
+//why use multiple template literals
+//- cleaner code
+//- easier string formatting
+//- supports multiple string
+
+//import and export
+//export allows us to share variable, funxtion or classes from one javascript file to another
+//import allows us to use those exported itemss in another javascript file
+//default and named export
+
+//Named export
+export default function add(a,b){
+    return a+b;
+}
+
+export default function substract(a,b){
+    return a-b;
+}
+//default is used to export the single items
+
+
+
+//-----------------------------------------------------------//
+//---------------------------REACT---------------------------//
+//-----------------------------------------------------------//
+
+//what is React.js?
+
+//It is a javascript library developed by Meta(facebook) for building
+// fast, interactive and reusable user interface(UI), especially single page applications
---
+++ script.js
@@ -0,0 +1,4 @@
+import{add,substract} from "./day13"
+
+console.log(add(10,120))
+console.log(substract(20,7))

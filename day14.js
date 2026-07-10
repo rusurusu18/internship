5df3c3bd76e9bff770605c2958ba043fadb11046
rushu <rushurushu721@gmail.com>
2026-07-10 19:47:25 +0545

    Update day14 and frontend App component

--- day14.js
+++ day14.js
@@ -1,9 +1,21 @@
 //props are used to pass data from parent component to child components
 //in props only can send data from parent to child
 
+import { useState } from "react";
+
 //prop are read only (no changes can be made)
 
 //App.jsx as parent
 //students.jsx
 //users.jsx
 //teacher.jsx
+
+
+//state is a built in react object used to store data that can change over time 
+//when the state change , react automatically updates the user interface(UI)
+//like: number in cart increases, notifications turning to 0
+
+//hooks: special rreact inbuilt function that lets function components use react features such as:
+//useState, useEffect, useContext, useMemo, etc
+
+const [value, setValue] = useState(0)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react' //rafce: short form for boiler template
+import React, { useState } from 'react' //rafce: short form for boiler template
 import User from './User.jsx'
 
 const App = () => {
+  const[count,setCount]=useState(0)
+  function increase(){
+    setCount(count+1);
+  }
   return (
     <>
-
+{/* 
       <User name="Rushu" age={21} college="IIC" Semester={5} />
-      <User skills={ ["HTML","CSS","JS"] } />
+      <User skills={ ["HTML","CSS","JS"] } /> */}
+
+      <h1>{count}</h1>
+      <button onClick={increase} className='bg-red-500 text-blue-800'>increment</button>
+
+
     </>
   )
 }
